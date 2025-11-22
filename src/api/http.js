135de0c2f8b8132dec47269c0be32a30e2fe2997
Franco Palavicino <fppalavicino@miuandes.cl>
2025-11-21 22:49:57 -0300

    fix: deploy

--- src/api/http.js
+++ src/api/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE || 
-           'https://image-uploader-backend-nine.vercel.app/api',
+           'https://image-uploader-backend-kysq.onrender.com/api',
 });
 
 export default http;
